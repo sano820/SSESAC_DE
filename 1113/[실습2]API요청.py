ad12d94b7fe94d177669d87f11bfcff3d4b68c35
sano820 <sano820@naver.com>
2025-11-13 17:43:07 +0900

    1113

--- 1113/[실습2]API요청.py
+++ 1113/[실습2]API요청.py
@@ -1,7 +1,7 @@
 import requests
+import pandas as pd
 from dotenv import load_dotenv
-import os
-import json
+import os, json
 
 load_dotenv()
 api_key = os.getenv("weather_api_key")
@@ -16,12 +16,39 @@ params = {
 }
 
 response = requests.get(base_url, params=params)
-if response.status_code == 200: # 요청에 성공했으면 
-    data = response.json() # 응답 바디 가져오기
-    print(json.dumps(data, ensure_ascii=False, indent =4))
-    
-    lon = data["coord"].get("lon","N/A")
-    lat = data["coord"].get("lon","N/A")
-    weather = data["weather"].get("description", "N/A")
-    wid_speed = data["wind"].get("speed", "N/A")
+if response.status_code == 200:
+    data = response.json()
+    print(json.dumps(data, ensure_ascii=False, indent=4))
+
+    # 필요한 데이터 추출
+    lon = data["coord"].get("lon", "N/A")
+    lat = data["coord"].get("lat", "N/A")
+    weather_main = data["weather"][0].get("main", "N/A")
+    weather_desc = data["weather"][0].get("description", "N/A")
+    temp = data["main"].get("temp", "N/A")
+    feels_like = data["main"].get("feels_like", "N/A")
+    humidity = data["main"].get("humidity", "N/A")
+    wind_speed = data["wind"].get("speed", "N/A")
     clouds = data["clouds"].get("all", "N/A")
+    country = data["sys"].get("country", "N/A")
+    city = data.get("name", "N/A")
+
+    # DataFrame 생성
+    df = pd.DataFrame([{
+        "city": city,
+        "country": country,
+        "lon": lon,
+        "lat": lat,
+        "weather_main": weather_main,
+        "weather_desc": weather_desc,
+        "temp(K)": temp,
+        "feels_like(K)": feels_like,
+        "humidity(%)": humidity,
+        "wind_speed(m/s)": wind_speed,
+        "clouds(%)": clouds
+    }])
+
+    # CSV로 저장
+    df.to_csv("./1113/outputs/weather_seoul.csv", index=False, encoding="utf-8-sig")
+else:
+    print("API 요청 실패:", response.status_code)
