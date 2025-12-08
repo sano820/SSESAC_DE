f734ee1440bb3bfe65a6f810ed9118df5cf3c924
sano820 <sano820@naver.com>
2025-12-08 17:53:57 +0900

    1208

--- 1208/client.py
+++ 1208/client.py
@@ -1,4 +1,7 @@
 import module as md
+import tensorflow
+from tensorflow import keras
+from keras.layers import Dropout
 
 result = md.AND(1,1)
 print(result)
@@ -6,3 +9,11 @@ print(result)
 md.plot_xy()
 md.plot_relu()
 md.plot_sigmoid()
+
+
+
+train_scaled, train_target, test_scaled, test_target = md.data()
+
+history, model = md.train(train_scaled, train_target, epochs=20, validation_split = 0.1, layer = keras.layers.Dropout(0.3))
+md.plot_history(history)
+md.eval(model, test_scaled, test_target)
--- 1208/module.py
+++ 1208/module.py
@@ -1,6 +1,10 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Flatten, Dropout
 
 def AND(x1, x2):
   result = 0.5*x1 + 0.5*x2 - 0.7
@@ -42,3 +46,49 @@ def plot_sigmoid():
   plt.show()
 
 
+
+# 실습하기
+# 데이터 불러와서 전처리
+def data():
+  (train_input, train_target), (test_input, test_target) = keras.datasets.fashion_mnist.load_data()
+  train_scaled = train_input / 255.0
+  test_scaled = test_input / 255.0
+  return train_scaled, train_target, test_scaled, test_target
+
+# 모델정의하기
+def model_fn(a_layer=None):
+  model = keras.Sequential()
+  model.add(keras.layers.Flatten(input_shape=(28, 28)))
+  model.add(keras.layers.Dense(100, activation='relu'))
+  if a_layer:
+    model.add(a_layer)
+  model.add(keras.layers.Dense(10, activation='softmax'))
+  return model
+
+# compile 및 학습
+def train(x,y, epochs, validation_split, layer=None):
+  model = model_fn(layer)
+  model.compile(
+    loss = 'sparse_categorical_crossentropy',
+    metrics = ['accuracy'],
+    optimizer = 'adam'
+  )
+  history = model.fit(x, y, epochs=epochs, verbose=2, validation_split = validation_split)
+  print(model.summary())
+  return history, model
+
+# 학습 loss 그래프 그리기
+def plot_history(history):
+  history.history.keys()
+
+
+  plt.plot(history.history['loss'])
+  plt.plot(history.history['val_loss'])
+  plt.xlabel('epoch')
+  plt.ylabel('loss')
+  plt.legend(['train', 'val'])
+  plt.show()
+
+# 모델 평가해보기.
+def eval(model, x,y):
+  print(model.evaluate(x,y))
