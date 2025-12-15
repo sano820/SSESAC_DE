148eecbbf573fca336acb785becdfb3720744038
sano820 <sano820@naver.com>
2025-12-15 22:22:27 +0900

    1215

--- Practice/module.py
+++ Practice/module.py
@@ -1,6 +1,17 @@
 from langchain_openai import ChatOpenAI
-
+from openai import OpenAI
 def test_openai():
     model = ChatOpenAI(model = 'gpt-4o', temperature=0)
     ai_message = model.invoke("안녕하세요.")
     print(ai_message)
+
+def respon(input = False):
+    message =[{'role':'system', 'content':'You are a 10years developer of AI', 'role':'user', 'content':input}]
+    client = OpenAI()
+    response = client.chat.completions.create(model = 'gpt-4o', messages= message)
+    return dict(response)
+
+def check_content(response):
+    content = response["choices"][0].message.content
+    return content
+
--- Practice/openai_client.py
+++ Practice/openai_client.py
@@ -3,3 +3,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 oam.test_openai()
+
+print("===")
+response = oam.respon(input="오늘 서울 날씨 알려줘")
+print(response)
+
+print("===")
+content = oam.check_content(response)
+print(content)
+
